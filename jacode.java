17bce4170072d249c6be3a7466289e1d945b8ca1
shambhavee <shambhaveesrivastava@gmail.com>
2022-10-10 12:27:00 +0530

    Shambhavee's code

---
+++ jacode.java
@@ -0,0 +1,63 @@
+class OperatorListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            String displayText = textfield.getText();
+            if (e.getActionCommand().equals("sin"))
+            {
+                textfield.setText("" + Math.sin(Double.valueOf(displayText).doubleValue()));
+                
+            }else
+            if (e.getActionCommand().equals("cos"))
+            {
+                textfield.setText("" + Math.cos(Double.valueOf(displayText).doubleValue()));
+                
+            }
+            else
+            if (e.getActionCommand().equals("log"))
+            {
+                textfield.setText("" + Math.log(Double.valueOf(displayText).doubleValue()));
+                
+            }
+            else if (e.getActionCommand().equals("C"))
+            {
+                textfield.setText("");
+            }
+ 
+            else
+            {
+                if (number)
+                {
+                    
+                    action();
+                    textfield.setText("");
+                    
+                }
+                else
+                {
+                    number = true;
+                    if (equalOp.equals("="))
+                    {
+                        op.setTotal(displayText);
+                    }else
+                    if (equalOp.equals("+"))
+                    {
+                        op.add(displayText);
+                    }
+                    else if (equalOp.equals("-"))
+                    {
+                        op.subtract(displayText);
+                    }
+                    else if (equalOp.equals("*"))
+                    {
+                        op.multiply(displayText);
+                    }
+                    else if (equalOp.equals("/"))
+                    {
+                        op.divide(displayText);
+                    }
+                    
+                    textfield.setText("" + op.getTotalString());
+                    equalOp = e.getActionCommand();
+                }
+            }
+        }
+    }
